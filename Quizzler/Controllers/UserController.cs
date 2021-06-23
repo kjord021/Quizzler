935f3a5c5f0df57414e59786db6d6c581a3b9b48
Kyle Jordan <kjord021@fiu.edu>
2021-06-22 22:45:58 -0400

    Created Route For Creating User

--- Quizzler/Controllers/UserController.cs
+++ Quizzler/Controllers/UserController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Quizzler.Contexts;
+using Quizzler.Logic;
+using Quizzler.Models;
 
 namespace Quizzler.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("/api/[controller]")]
     public class UserController : Controller
     {
         private readonly UserDbContext _context;
@@ -16,6 +19,40 @@ namespace Quizzler.Controllers
         }
 
         //Create
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(string username, string password, string confPassword, string emailAddress)
+        {
+
+            try
+            {
+
+                User newUser = new User()
+                {
+                    UserName = username,
+                    Password = password,
+                    Email = emailAddress
+                };
+
+                var validInput = EnsureValidInput.EnsureUserData(newUser, confPassword);
+
+                if (validInput)
+                {
+                    await _context.AddAsync(newUser);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    return BadRequest("Invalid User Data.");
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
+            return Ok();
+        }
 
         //Read
 
